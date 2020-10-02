5602694da59cef44200c069519c3a9bb857532fa
vincent <vincent.bulfon@gmail.com>
2020-10-02 16:08:40 +0200

    Added relation between Matches and Teams :construction:

--- app/Models/Match.php
+++ app/Models/Match.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Match extends Model
 {
     use HasFactory;
+
+    /**
+     * return the relation between matches and teams
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function teams()
+    {
+        return $this->belongsToMany('App\Models\Team', 'Participations')->withPivot('is_home', 'goals');
+    }
+
+    /**
+     * return the boolean if team is at home or not
+     * @return string
+     */
+    public function getHomeTeamAttribute()
+    {
+        return $this->teams->filter(function ($team) {
+            return $team->pivot->is_home === 1;
+        })->first()->name;
+    }
+
+    public function getAwayTeamAttribute()
+    {
+        return $this->team
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     use HasFactory;
+
+    /**
+     * return the relation between matches and teams
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function matches()
+    {
+        return  $this->belongsToMany('App\Models\Match', 'Participations')->withPivot('is_home', 'goals');
+    }
 }
